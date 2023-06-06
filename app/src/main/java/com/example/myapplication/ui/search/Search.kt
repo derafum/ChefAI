2d9dbd633f07d6bc87b80bc25ba74c8746fb408b
ApelsinDev <nik_zhur_serg@mail.ru>
2023-06-06 08:50:55 +0400

    некоторые изменения в Search

--- app/src/main/java/com/example/myapplication/ui/search/Search.kt
+++ app/src/main/java/com/example/myapplication/ui/search/Search.kt
@@ -1,12 +1,20 @@
 package com.example.myapplication.ui.search
 
+import android.content.ClipData
+import android.content.ClipData.Item
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentHomeBinding
+import com.example.myapplication.databinding.FragmentSearchBinding
 
 class Search : Fragment() {
 
@@ -15,13 +23,65 @@ class Search : Fragment() {
     }
 
     private lateinit var viewModel: SearchViewModel
+    private lateinit var searchView: SearchView
+    private lateinit var reView3: RecyclerView
+    private lateinit var binding: FragmentSearchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        /*
+
+        searchView = binding.searchView
+
+        searchView.clearFocus()
+
+
+
+        searchView.setOnQueryTextListener{object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                filterList(newText)
+                return true;
+            }
+
+        }}
+
+
+
+        reView3 = binding.reView3
+        reView3.setHasFixedSize(true)
+        binding.reView3.layoutManager = LinearLayoutManager(context)
+        reView3.layoutManager = LinearLayoutManager(activity, RecyclerView.HORIZONTAL, false)
+*/
+
+
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
+/*
+    private fun filterList(text: String) {
+        List<Item> filteredList = new ArrayList<>();
+        for (Item item: itemList){
+            if (item.getItemName().toLowerCase().contains(text.toLowerCase())){
+                filteredList.add(item);
+            }
+        }
+
+        if (filteredList.isEmpty()){
+            Toast.makeText(this, "No data found", Toast.LENGTH_SHORT).show();
+
+        }else{
+            itemAdapter.setFilteredList(filteredList);
+        }
+
+    }
+
+ */
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
