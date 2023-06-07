474cf86e2dbeda5270103da4105aa985622e0a22
ApelsinDev <nik_zhur_serg@mail.ru>
2023-06-07 18:08:17 +0400

    переименовал файл шефу не нравится

--- app/src/main/java/com/example/myapplication/CardFragment.kt
+++ app/src/main/java/com/example/myapplication/CardFragment.kt
@@ -34,7 +34,7 @@ class CardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_card, container, false)
+        return inflater.inflate(R.layout.activity_card, container, false)
     }
 
     companion object {
