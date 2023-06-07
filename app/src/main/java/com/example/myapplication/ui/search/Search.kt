dbbd6b75d0989f479a4280c0fa9433b7fd8fc5a6
George <krasavingeorg@gmail.com>
2023-06-07 06:30:14 +0400

    Добавил и подключил вертикальный слайдер

--- app/src/main/java/com/example/myapplication/ui/search/Search.kt
+++ app/src/main/java/com/example/myapplication/ui/search/Search.kt
@@ -1,37 +1,23 @@
 package com.example.myapplication.ui.search
-
-import android.content.ClipData
-import android.content.ClipData.Item
+import DatabaseHelper
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.DatabaseHelper
-import com.example.myapplication.R
+import com.example.myapplication.Recipe
 import com.example.myapplication.RecipeAdapter
-import com.example.myapplication.databinding.ActivityMainBinding
-import com.example.myapplication.databinding.FragmentHomeBinding
 import com.example.myapplication.databinding.FragmentSearchBinding
-import com.example.myapplication.ui.analize.Analyze
+
 
 class Search : Fragment() {
-    private lateinit var binding: FragmentHomeBinding
+
     private val adapter = RecipeAdapter()
-    private val imageIdList = listOf(
-        R.drawable.recipe1,
-        R.drawable.recipe2,
-        R.drawable.recipe3,
-        R.drawable.recipe4,
-        R.drawable.recipe5,
-        R.drawable.recipe6,
-    )
-    private var index = 0
+
 
 
     companion object {
@@ -55,40 +41,45 @@ class Search : Fragment() {
 
         binding.searchView.clearFocus()
 
-        binding.searchView.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
+        binding.searchView.setOnQueryTextListener(object :
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                // Выполните действия при отправке текста запроса
                 query?.let { logSearchText(it) }
                 return true
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                // Выполните действия при изменении текста запроса
                 newText?.let { filterList(it) }
                 return true
             }
         })
 
         binding.reView3.setHasFixedSize(true)
-        binding.reView3.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+        binding.reView3.layoutManager =
+            LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        binding.reView3.adapter = adapter // Установка адаптера для RecyclerView
     }
 
     private fun logSearchText(query: String) {
-        // Выведите текст запроса в лог
-        Log.d(TAG, "Search Query: ${query}")
-        // Получите данные рецепта по слову запроса
-        val dbHelper = DatabaseHelper(requireActivity())
-        val recipeDataList = dbHelper.getRecipeDataByWord(query)
+        Log.d(TAG, "Search Query: $query")
 
-        // Выведите данные рецепта в лог
+        val dbHelper = DatabaseHelper(requireActivity())
+        val recipeDataList = dbHelper.getRecipeDataByName(query)
+
+        adapter.recipeList.clear()
+        adapter.notifyDataSetChanged()
+
         for (recipeData in recipeDataList) {
             Log.d(TAG, "Recipe Name: ${recipeData.name}")
             Log.d(TAG, "Time: ${recipeData.time}")
             Log.d(TAG, "Image: ${recipeData.img}")
-            Log.d(TAG, "Amount of Servings: ${recipeData.amountServings}")
             Log.d(TAG, "Energy: ${recipeData.energy}")
             Log.d(TAG, "Ingredients: ${recipeData.ingredients}")
             Log.d(TAG, "Instructions: ${recipeData.instructions}")
+            Log.d(TAG, "Numbers: ${recipeData.number}")
+
+            val recipe = Recipe(recipeData.img, recipeData.name, recipeData.time)
+            adapter.addRecipe(recipe)
         }
     }
 
