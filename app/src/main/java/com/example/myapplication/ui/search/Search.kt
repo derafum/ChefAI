aadbe27c9fdbfa2c142b85b15147b93b578347f0
George <krasavingeorg@gmail.com>
2023-06-07 04:51:24 +0400

    Вывожу в log функцию функцию для поиска данных в бд по слову

--- app/src/main/java/com/example/myapplication/ui/search/Search.kt
+++ app/src/main/java/com/example/myapplication/ui/search/Search.kt
@@ -3,90 +3,88 @@ package com.example.myapplication.ui.search
 import android.content.ClipData
 import android.content.ClipData.Item
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SearchView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.DatabaseHelper
 import com.example.myapplication.R
-import com.example.myapplication.databinding.FragmentHomeBinding
 import com.example.myapplication.databinding.FragmentSearchBinding
+import com.example.myapplication.ui.analize.Analyze
 
 class Search : Fragment() {
 
     companion object {
         fun newInstance() = Search()
+        private const val TAG = "Search"
     }
 
     private lateinit var viewModel: SearchViewModel
-    private lateinit var searchView: SearchView
-    private lateinit var reView3: RecyclerView
     private lateinit var binding: FragmentSearchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        /*
-
-        searchView = binding.searchView
-
-        searchView.clearFocus()
-
-
-
-        searchView.setOnQueryTextListener{object: SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(p0: String?): Boolean {
-                TODO("Not yet implemented")
-            }
-
-            override fun onQueryTextChange(newText: String): Boolean {
-                filterList(newText)
-                return true;
-            }
-
-        }}
-
-
-
-        reView3 = binding.reView3
-        reView3.setHasFixedSize(true)
-        binding.reView3.layoutManager = LinearLayoutManager(context)
-        reView3.layoutManager = LinearLayoutManager(activity, RecyclerView.HORIZONTAL, false)
-*/
-
-
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
     }
-/*
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.searchView.clearFocus()
+
+        binding.searchView.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // Выполните действия при отправке текста запроса
+                query?.let { logSearchText(it) }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // Выполните действия при изменении текста запроса
+                newText?.let { filterList(it) }
+                return true
+            }
+        })
+
+        binding.reView3.setHasFixedSize(true)
+        binding.reView3.layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+    }
+
+    private fun logSearchText(query: String) {
+        // Выведите текст запроса в лог
+        Log.d(TAG, "Search Query: ${query}")
+        // Получите данные рецепта по слову запроса
+        val dbHelper = DatabaseHelper(requireActivity())
+        val recipeDataList = dbHelper.getRecipeDataByWord(query)
+
+        // Выведите данные рецепта в лог
+        for (recipeData in recipeDataList) {
+            Log.d(TAG, "Recipe Name: ${recipeData.name}")
+            Log.d(TAG, "Time: ${recipeData.time}")
+            Log.d(TAG, "Image: ${recipeData.img}")
+            Log.d(TAG, "Amount of Servings: ${recipeData.amountServings}")
+            Log.d(TAG, "Energy: ${recipeData.energy}")
+            Log.d(TAG, "Ingredients: ${recipeData.ingredients}")
+            Log.d(TAG, "Instructions: ${recipeData.instructions}")
+        }
+    }
+
     private fun filterList(text: String) {
-        List<Item> filteredList = new ArrayList<>();
-        for (Item item: itemList){
-            if (item.getItemName().toLowerCase().contains(text.toLowerCase())){
-                filteredList.add(item);
-            }
-        }
-
-        if (filteredList.isEmpty()){
-            Toast.makeText(this, "No data found", Toast.LENGTH_SHORT).show();
-
-        }else{
-            itemAdapter.setFilteredList(filteredList);
-        }
-
+        // Выполните фильтрацию списка на основе текста запроса
+        // и обновите адаптер списка
     }
-
- */
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
         // TODO: Use the ViewModel
     }
-
 }
